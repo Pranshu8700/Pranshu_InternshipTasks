f5bfd97251455d993a8e90c741d6c1dc50980562
Pranshu Joshi <pj87008700@gmail.com>
2021-06-04 12:17:19 +0530

    Day - 3 tasks

---
+++ Day-3/smallest.py
@@ -0,0 +1,10 @@
+  
+print("Enter the 1st number:")
+num1=int(input())
+print("Enter the 2nd number:")
+num2=int(input())
+
+if num1<num2:
+    print("1st number->", num1, ", is smallest." )
+else:
+    print("2nd number->", num2, ",is smallest.")
